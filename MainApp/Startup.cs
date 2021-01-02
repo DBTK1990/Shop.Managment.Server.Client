2d7f51bc3f02b7553a8d07d8821ad7d52e18386b
dudu.b <dudu.b@comstar.co.il>
2021-01-02 17:22:40 +0200

    remove: Swagger on development

--- MainApp/Startup.cs
+++ MainApp/Startup.cs
@@ -28,10 +28,7 @@ namespace MainApp
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MainApp", Version = "v1" });
-            });
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +37,6 @@ namespace MainApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MainApp v1"));
             }
 
             app.UseHttpsRedirection();
@@ -49,7 +44,7 @@ namespace MainApp
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

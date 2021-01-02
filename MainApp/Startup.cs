7cc853351698ddf6b3de34ad6139d6729fcaa10b
dudu.b <dudu.b@comstar.co.il>
2021-01-02 17:49:50 +0200

    remove unused using

--- MainApp/Startup.cs
+++ MainApp/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using BL.Setup;
 using Security.Setup;
 namespace MainApp

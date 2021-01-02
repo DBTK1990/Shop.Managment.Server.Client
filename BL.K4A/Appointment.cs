3cb9a10dd88a1ff05849f7fc3ad808d2ebfbedb3
dudu.b <dudu.b@comstar.co.il>
2021-01-03 00:04:43 +0200

    add: username to appointment model

--- BL.K4A/Appointment.cs
+++ BL.K4A/Appointment.cs
@@ -2,17 +2,20 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace BL
 {
     public class Appointment
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public DateTime Date_Set { get; set; }
-        [Required]
-        public int UserId { get; set; }
+        [JsonIgnore]
+        public string UserId { get; set; }
+        public string Username { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime Date_Inserted { get; set; }
     

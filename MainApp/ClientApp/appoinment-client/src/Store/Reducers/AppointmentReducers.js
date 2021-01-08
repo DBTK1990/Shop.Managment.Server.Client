110880573d7203828a90fd1467fc89cbc0af3133
dudu.b <dudu.b@comstar.co.il>
2021-01-08 18:33:16 +0200

    add more refresh token logic

--- MainApp/ClientApp/appoinment-client/src/Store/Reducers/AppointmentReducers.js
+++ MainApp/ClientApp/appoinment-client/src/Store/Reducers/AppointmentReducers.js
@@ -1,16 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import appointment_api from "../../Services/AppointmentService";
-
+import { checkConnection, registerError } from "../Helpers/reduxUtil";
 export const appointmentThunk = {
   pager: createAsyncThunk(
     "appointment/pager",
     async ({ page_num, filter_o, order_o }, thunkAPI) => {
       try {
+        await checkConnection(thunkAPI);
         const { tokenResponse, pagerQuery } = thunkAPI.getState().token;
-        var api = new appointment_api(tokenResponse.token);
-        var filter = filter_o ? filter_o : pagerQuery.filter;
-        var order = order_o ? order_o : pagerQuery.order;
-        var response = await api.getListByPage(page_num, filter, order);
+        let api = new appointment_api(tokenResponse.token);
+        let filter = filter_o ? filter_o : pagerQuery.filter;
+        let order = order_o ? order_o : pagerQuery.order;
+        let response = await api.getListByPage(page_num, filter, order);
         return {
           data: response.data,
           page_num,
@@ -22,7 +23,8 @@ export const appointmentThunk = {
   ),
   details: createAsyncThunk("appointment/details", async (id, thunkAPI) => {
     try {
-      const { tokenResponse, pagerQuery } = thunkAPI.getState().token;
+      await checkConnection(thunkAPI);
+      const { tokenResponse } = thunkAPI.getState().token;
       var api = new appointment_api(tokenResponse.token);
       var response = await api.getDetailsById(id);
       return response.data;
@@ -32,6 +34,7 @@ export const appointmentThunk = {
   }),
   create: createAsyncThunk("appointment/create", async (date, thunkAPI) => {
     try {
+      await checkConnection(thunkAPI);
       const {
         tokenResponse,
         pagerQuery,
@@ -54,6 +57,7 @@ export const appointmentThunk = {
   }),
   edit: createAsyncThunk("appointment/edit", async ({ id, date }, thunkAPI) => {
     try {
+      await checkConnection(thunkAPI);
       const {
         tokenResponse,
         pagerQuery,
@@ -76,6 +80,7 @@ export const appointmentThunk = {
   }),
   delete: createAsyncThunk("appointment/delete", async (id, thunkAPI) => {
     try {
+      await checkConnection();
       var { token, pagerQuery, table_page_num } = thunkAPI.getState();
       var api = new appointment_api(token.tokenResponse.token);
       var response = await api.delete(id);
@@ -102,35 +107,20 @@ const appointmentExtraReducer = {
     state.table_page_num = action.payload.page_num;
   },
   [appointmentThunk.pager.rejected]: (state, action) => {
-    // Add user to the state array
-    var data = action.payload.response;
-    state.error_model.show = true;
-    state.error_model.heading = `${data.title}`;
-    state.error_model.body = data.error ?? data.message;
+    let data = action.payload.response.data;
+    registerError(state, data);
   },
   [appointmentThunk.delete.rejected]: (state, action) => {
-    // Add user to the state array
-    var data = action.payload.response.data;
-
-    state.error_model.show = true;
-    state.error_model.heading = `${data.title}`;
-    state.error_model.body = data.error ?? data.message;
+    let data = action.payload.response.data;
+    registerError(state, data);
   },
   [appointmentThunk.edit.rejected]: (state, action) => {
-    // Add user to the state array
-    var data = action.payload.response.data;
-
-    state.error_model.show = true;
-    state.error_model.heading = `${data.title}`;
-    state.error_model.body = data.error ?? data.message;
+    let data = action.payload.response.data;
+    registerError(state, data);
   },
   [appointmentThunk.create.rejected]: (state, action) => {
-    // Add user to the state array
-    var data = action.payload.response.data;
-
-    state.error_model.show = true;
-    state.error_model.heading = `${data.title}`;
-    state.error_model.body = data.error ?? data.message;
+    let data = action.payload.response.data;
+    registerError(state, data);
   },
 };
 export default appointmentExtraReducer;

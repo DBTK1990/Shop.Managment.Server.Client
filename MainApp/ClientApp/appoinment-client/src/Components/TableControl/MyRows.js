78e66b36edf5cf195b4447e74051be6149c1988d
dudu.b <dudu.b@comstar.co.il>
2021-01-08 17:23:38 +0200

    remove unnecessary signture

--- MainApp/ClientApp/appoinment-client/src/Components/TableControl/MyRows.js
+++ MainApp/ClientApp/appoinment-client/src/Components/TableControl/MyRows.js
@@ -2,27 +2,27 @@ import React, { Component } from "react";
 import OptionsCol from "./OptionsCol";
 
 export default class MyRows extends Component {
-    constructor(props) {
-        super();
-    }
-   
-    render() {
-        const { data_rows, this_user } = this.props;
-        return data_rows
-            ? data_rows.map((el) => {
-                return (
-                    <tr align="center" key={el.id}>
-                        <td>{el.username}</td>
-                        <td >{new Date(el.date_Set).toDateString('he')}</td>
-                        <td >{new Date(el.date_Set).toLocaleTimeString('he')}</td>
-                        <OptionsCol
-                            {...this.props}
-                            assignId={el.id}
-                            restricted={el.username === this_user}
-                        />
-                    </tr>
-                );
-            })
-            : "";
-    }
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    const { data_rows, this_user } = this.props;
+    return data_rows
+      ? data_rows.map((el) => {
+          return (
+            <tr align="center" key={el.id}>
+              <td>{el.username}</td>
+              <td>{new Date(el.date_Set).toDateString()}</td>
+              <td>{new Date(el.date_Set).toLocaleTimeString("he")}</td>
+              <OptionsCol
+                {...this.props}
+                assignId={el.id}
+                restricted={el.username === this_user}
+              />
+            </tr>
+          );
+        })
+      : "";
+  }
 }
